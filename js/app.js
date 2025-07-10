a6a553743c76e9bf8b490474f9fde8a3247190ee
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-07-10 20:07:43 +0300

    define each column as a classes and then assign them into cached elements as an arrays

--- js/app.js
+++ js/app.js
@@ -8,7 +8,15 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+//Array from means conveert the nodelist into an array
+const column1 = Array.from(document.querySelectorAll('.column1'))
+console.log(column1)
+const column2 = Array.from(document.querySelectorAll('.column2'))
+console.log(column2)
+const column3 = Array.from(document.querySelectorAll('.column3'))
+console.log(column3)
+const column4 = Array.from(document.querySelectorAll('.column4'))
+console.log(column4)
 
 
 /*-------------------------------- Functions --------------------------------*/
