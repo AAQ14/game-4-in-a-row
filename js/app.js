480c3d83defce76e513a31a859eab1c3f6b370f9
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-07-10 22:40:36 +0300

    adding logic to column2 based on which turn is it switch the color

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 //i have to allow the chip to be fall into the empty div by style
 //i have to let the chip to be fall when a column being clicked && it must be at the empty string
 // i have to let the chips being switched based on which turn is it
+//increment the turn to be based on if(turn%==2)than(player1 turn)else than (player 2)
 
 
 /*-------------------------------- Constants --------------------------------*/
@@ -43,11 +44,44 @@ column2.forEach((item) => {
     item.addEventListener('click', event => {
         //replace the console with the picture to be fall into the circle
         console.log("column2 clicked")
-        if (turn % 2 === 0)
-            item.classList.add('redChip')
-        else
-            item.classList.add('yellowChip')
-        turn++
+        console.log(column2[3])
+        if (column2[0].innerHTML === '' && column2[3].innerHTML !== '' && column2[2].innerHTML !== '' && column2[1].innerHTML !== '') {
+            if (turn % 2 === 0)
+                column2[0].classList.add('redChip')
+            else
+                column2[0].classList.add('yellowChip')
+            turn++
+            column2[0] = ' '
+        }
+
+        if (column2[1].innerHTML === '' && column2[3].innerHTML !== '' && column2[2].innerHTML !== '') {
+            if (turn % 2 === 0)
+                column2[1].classList.add('redChip')
+            else
+                column2[1].classList.add('yellowChip')
+            turn++
+            column2[1] = ' '
+        }
+
+        if (column2[2].innerHTML === '' && column2[3].innerHTML !== '') {
+            if (turn % 2 === 0)
+                column2[2].classList.add('redChip')
+            else
+                column2[2].classList.add('yellowChip')
+            turn++
+            column2[2] = ' '
+        }
+
+
+        if (column2[3].innerHTML === '') {
+            if (turn % 2 === 0)
+                column2[3].classList.add('redChip')
+            else
+                column2[3].classList.add('yellowChip')
+            turn++
+            column2[3] = ' '
+        }
+
     })
 })
 column3.forEach((item) => {
