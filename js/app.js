2626e805bd02ca3a695514ffb1a065f44d0b16ae
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-07-10 21:32:59 +0300

    style the images and testing them by adding class elm at specific index

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+//i have to do an array for each column ✔
+//i may introduce the first column as a class for example ✔
+//i have to insert chips pictures and fix the size
+//i have to allow the chip to be fall into the empty div
+
 /*-------------------------------- Constants --------------------------------*/
-//i have to do an array for each column
-//i may introduce the first column as a class for example
+
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -15,16 +19,24 @@ const column3 = Array.from(document.querySelectorAll('.column3'))
 const column4 = Array.from(document.querySelectorAll('.column4'))
 const blocker = document.querySelector('.blocker')
 
+let idx = 1
 
 /*-------------------------------- Functions --------------------------------*/
-
-
+function addRedChip(){
+   column1[idx].classList.add('redChip')
+}
+function addYellowChip(){
+    column2[idx].classList.add('yellowChip')
+}
+addRedChip()
+addYellowChip()
 
 /*----------------------------- Event Listeners -----------------------------*/
 column1.forEach((item) => {
     item.addEventListener('click', event => {
         //replace the console with the picture to be fall into the circle
         console.log("column1 clicked")
+
     })
 })
 column2.forEach((item) => {
